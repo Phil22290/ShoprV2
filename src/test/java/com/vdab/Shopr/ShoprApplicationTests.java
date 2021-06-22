1dc49ad3ea396fb1fa4046b5990a918a82da9bc8
Phil <philippe.hermans@hotmail.be>
2021-06-22 16:36:43 +0200

    initial commit

---
+++ src/test/java/com/vdab/Shopr/ShoprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdab.Shopr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
